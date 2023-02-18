219b1c9b8eea2e682d2f2519cf4aae59aef08c96
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-02-19 01:40:53 +0200

    add file 'LoginScreen'

--- App.js
+++ App.js
@@ -1,25 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { LoginScreen } from "./screens/auth/LoginScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js</Text>
-      <Text style={styles.text}>to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <LoginScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "aqua",
-    fontSize: 24,
-  },
-});
