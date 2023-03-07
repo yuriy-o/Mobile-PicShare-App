c8e337f2817f099dbd1cb60319cfbbcef5d47fb2
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-03-07 20:41:54 +0200

    hide title

--- screens/mainScreen/PostsScreen.js
+++ screens/mainScreen/PostsScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { moduleName } from "react-native";
+//для створення вложених скринов
 import { createStackNavigator } from "@react-navigation/stack";
 import { DefaultScreenPosts } from "../nestedScreens/DefaultScreenPosts";
 import { CommentsScreen } from "../nestedScreens/CommentsScreen";
@@ -10,9 +11,14 @@ const NestedScreen = createStackNavigator();
 export const PostsScreen = () => {
   return (
     <NestedScreen.Navigator>
+      {/* //в Навігатор вкладуємо необхідну к-ть скринів */}
       <NestedScreen.Screen
-        name="DefaultScreen"
+        name="DefaultScreenPosts"
         component={DefaultScreenPosts}
+        options={{
+          headerShown: false, // Приховати заголовок екрана
+          // tabBarVisible: false, // Приховати відображення вкладок
+        }}
       />
       <NestedScreen.Screen name="Comments" component={CommentsScreen} />
       <NestedScreen.Screen name="Map" component={MapScreen} />
