681aef9dfafcf98c9249fa8f3b1674ef3348509c
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-03-18 23:29:40 +0200

    upd config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,16 +1,32 @@
-// import firebase from "firebase/app";
-// // or
+//? firebaseConfig-v0;
 // import * as firebase from "firebase";
 // import "firebase/auth";
 // import "firebase/storage";
 // import "firebase/firestore";
 
-//? firebaseConfig-v1;
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB2aYvslUEqeNOwvQPj3u20NQdGjQ1hxZo",
+//   authDomain: "react-nativ-hw-01.firebaseapp.com",
+//   projectId: "react-nativ-hw-01",
+//   storageBucket: "react-nativ-hw-01.appspot.com",
+//   messagingSenderId: "816373487861",
+//   appId: "1:816373487861:web:8176601f3e40c387500dbc",
+//   measurementId: "G-4LM9WXTJSG",
+// };
+// // Initialize Firebase
+// export default firebase.initializeApp(firebaseConfig);
+
+// //? firebaseConfig-v1-old;
 
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-import "firebase/storage";
+import "firebase/compat/storage";
 import "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2aYvslUEqeNOwvQPj3u20NQdGjQ1hxZo",
@@ -26,6 +42,31 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+//? firebaseConfig-v1-new;
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/storage";
+// import "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB2aYvslUEqeNOwvQPj3u20NQdGjQ1hxZo",
+//   authDomain: "react-nativ-hw-01.firebaseapp.com",
+//   projectId: "react-nativ-hw-01",
+//   storageBucket: "react-nativ-hw-01.appspot.com",
+//   messagingSenderId: "816373487861",
+//   appId: "1:816373487861:web:8176601f3e40c387500dbc",
+//   measurementId: "G-4LM9WXTJSG",
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// export default firebase;
 
 //! Other imports and exports
 //? firebaseConfig-v2;
