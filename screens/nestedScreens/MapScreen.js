13928a8332860fa1c55e07b5a3ae794544de67b6
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-03-18 23:35:20 +0200

    upd file MapScreen

--- screens/nestedScreens/MapScreen.js
+++ screens/nestedScreens/MapScreen.js
@@ -2,7 +2,18 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
-export const MapScreen = () => {
+export const MapScreen = ({ route }) => {
+  // console.log("MapScreen__route", route);
+  // console.log("MapScreen__route.params", route.params);
+  console.log("MapScreen__route.params.posts", route.params.posts);
+  console.log(
+    "MapScreen__route.params.posts.location",
+    route.params.posts.location
+  );
+  // console.log("MapScreen__latitude", route.params.location.coords.latitude);
+  // console.log("MapScreen__longitude", route.params.location.coords.longitude);
+  // console.log("MapScreen__navigation.navigate", navigation.navigate);
+
   return (
     <View style={styles.container}>
       <MapView
@@ -16,14 +27,34 @@ export const MapScreen = () => {
           longitudeDelta: 0.006,
         }}
       >
-        <Marker
+        {/* <Marker
           coordinate={{ latitude: 50.516339, longitude: 30.602185 }}
           title={item.description}
-        />
+        /> */}
       </MapView>
     </View>
   );
 };
+
+// const MapScreen = ({ route }) => {
+//   console.log("route.params.location", route.params.location);
+//   const { longitude, latitude } = route.params.location;
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         style={{ flex: 1 }}
+//         initialRegion={{
+//           latitude,
+//           longitude,
+//           latitudeDelta: 0.001,
+//           longitudeDelta: 0.006,
+//         }}
+//       >
+//         <Marker coordinate={{ latitude, longitude }} title="travel photo" />
+//       </MapView>
+//     </View>
+//   );
+// };
 
 const styles = StyleSheet.create({
   container: {
