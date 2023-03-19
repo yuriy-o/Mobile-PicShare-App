2e1d5fe4b3ea2747dc926e01f2d416ff7a5039e4
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-03-20 00:40:19 +0200

    upload data from firebase

--- screens/nestedScreens/DefaultScreenPosts.js
+++ screens/nestedScreens/DefaultScreenPosts.js
@@ -3,104 +3,68 @@ import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 import "firebase/firestore";
-import db from "../../firebase/config";
-// import { collection, getDocs, doc, onSnapshot } from "firebase/firestore";
+// import db from "../../firebase/config";
+import { db } from "../../firebase/config";
+import { collection, onSnapshot } from "firebase/firestore";
 
 export const DefaultScreenPosts = ({ navigation, route }) => {
   const [posts, setPosts] = useState([]);
 
-  const getAllPost = async () => {
-    await db
-      .firestore()
-      .collection("posts")
-      .onSnapshot((data) =>
-        setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-      );
+  const getAllPosts = async () => {
+    await onSnapshot(collection(db, "posts"), (data) => {
+      setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
   };
 
   useEffect(() => {
-    console.log("d");
-    getAllPost();
+    getAllPosts();
   }, []);
-
-  //! v3
-  // const getAllPost = async () => {
-  //   const querySnapshot = await getDocs(collection(db, "posts"));
-  //   setPosts(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  // };
-
-  // //! v4
-  // const getAllPost = async () => {
-  //   await db
-  //     .firestore()
-  //     .collection("posts")
-  //     .onSnapshot((data) =>
-  //       setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-  //     );
-  // };
-
-  // useEffect(() => {
-  //   getAllPost();
-  // }, []);
-
-  //! v6
-  // const getAllPost = useCallback(async () => {
-  //   try {
-  //     const data = await db.firestore().collection("posts").get();
-  //     setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  //   } catch (error) {
-  //     console.error("Error getting posts: ", error);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   getAllPost();
-  // }, [getAllPost]);
-
-  const viewMap = () => {
-    navigation.navigate("Map", { location: item.location });
-  };
-  const viewComment = () => {
-    navigation.navigate("Comment", { postId: item.id });
-  };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={posts}
-        keyExtractor={(item, indx) => indx.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.postContainer}>
-            <Image source={{ uri: item.photoURL }} style={styles.image} />
-            {/* <Image source={{ uri: item.photo }} style={styles.image} /> */}
-            <Text style={styles.comment}>{item.comment}</Text>
-            <View style={styles.chatLocationContainer}>
-              <Ionicons
-                style={styles.chatIcon}
-                name="chatbox-outline"
-                size={24}
-                color="#BDBDBD"
-                onPress={() => {
-                  viewComment();
-                }}
-              />
-
-              <View style={styles.locationContainer}>
-                <Text style={styles.locationText}>{item.locationName}</Text>
+      {posts && (
+        <FlatList
+          data={posts}
+          keyExtractor={(item, indx) => indx.toString()}
+          // keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.postContainer}>
+              <Image source={{ uri: item.photoURL }} style={styles.image} />
+              <Text style={styles.comment}>{item.comment}</Text>
+              <View style={styles.chatLocationContainer}>
                 <Ionicons
-                  style={styles.locationIcon}
-                  name="location-outline"
+                  style={styles.chatIcon}
+                  name="chatbox-outline"
                   size={24}
                   color="#BDBDBD"
                   onPress={() => {
-                    viewMap();
+                    navigation.navigate("Comment", {
+                      postId: item.id,
+                      comment: item.comment,
+                    });
                   }}
                 />
+
+                <View style={styles.locationContainer}>
+                  <Text style={styles.locationText}>{item.locationName}</Text>
+                  <Ionicons
+                    style={styles.locationIcon}
+                    name="location-outline"
+                    size={24}
+                    color="#BDBDBD"
+                    onPress={() => {
+                      navigation.navigate("Map", {
+                        location: item.location,
+                        locationName: item.locationName,
+                      });
+                    }}
+                  />
+                </View>
               </View>
             </View>
-          </View>
-        )}
-      />
+          )}
+        />
+      )}
     </View>
   );
 };
