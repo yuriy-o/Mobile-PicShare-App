9b72a7f39b057e2966724426b4a331131c28ad3c
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-03-21 14:22:01 +0200

    Removes unnecessary pages

--- v App copy.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { useRoute } from "./component/routers";
-
-export default function App() {
-  const routing = useRoute(1);
-
-  return <NavigationContainer>{routing}</NavigationContainer>;
-}
